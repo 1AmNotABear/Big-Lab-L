10bfc3f812228f48c4c9a8e5442b73c88c81501f
lailamirk <laila.mirkazemi@gmail.com>
2026-07-20 15:40:36 +1000

    Add files via upload

---
+++ src/doorbell.c
@@ -0,0 +1,90 @@
+#include "lpc24xx.h"
+#include "doorbell.h"
+
+#define DOORBELL_BTN (1 << 10) // P0.10 for push button corresponding to the doorbell
+
+struct tone {
+	int duration;
+	int pitch;
+	int volume;
+};
+
+struct tone chime_data[] = {
+    {10, 2725, 0x300}, // (duration, period, and volume)
+    {10, 2725, 0x300}, // (duration, period, and volume)
+    {10, 2725, 0x300}, // (duration, period, and volume)
+    {10, 2725, 0x300}, // (duration, period, and volume)
+    {10, 2725, 0x300} // (duration, period, and volume)
+};
+
+// Function Prototype
+void ringDoorbell(unsigned int duration, int period, int vol);
+void udelay(unsigned int delay_in_us);
+
+// Detects when the doorbell is actually pushed
+void pushDoorbell(HomeState *state) {
+
+    int rate = 5200; // Sets speed that the sounds plays
+
+    // Setup DAC
+	PINSEL1 &= ~(3 << 20); // Clears bits 21:20
+	PINSEL1 |= (2 << 20); // Sets bits 21:20 to 0b10 (AOUT)
+
+    unsigned char btn_pressed = (FIO0PIN & DOORBELL_BTN) ? 1 : 0; // Whether button pushed or not
+
+    if (btn_pressed == 1) {
+
+        for (int i = 0; i < 5; i++) {
+            ringDoorbell(rate * chime_data[i].duration, chime_data[i].pitch, chime_data[i].volume); // Rings for 3 seconds with period of 1 second
+        }
+    }
+
+}
+
+
+// Plays the chime for the doorbell considering duration and period
+void ringDoorbell(unsigned int duration, int period, int vol) {
+
+    unsigned int elapsed_time = 0;
+
+    // High for half period and low for half period (square wave)
+	unsigned int half_period = (unsigned int)period / 2;
+
+    while (elapsed_time < duration) {
+		
+		DACR = ((unsigned int)vol << 6); // High for half-period
+		udelay(half_period);
+		
+		DACR = 0; // Low for half-period
+		udelay(half_period);		
+		
+		elapsed_time += period; // after one complete period done, adds on to track time
+		
+	}
+
+	DACR = 0; // output silent when done
+
+}
+
+
+// Delays processor for 'delay_in_us' number of microseconds
+void udelay(unsigned int delay_in_us) {
+	if (delay_in_us == 0) return;
+	
+	TOTCR = 0x02; // Reset and disable timer
+	
+	TOPR = (Fpclk / 1000000) - 1; // ticks per microsecond accounting for (PR + 1)
+
+	TOTCR = 0x1; // sets enable high and reset low
+	
+	while (T0TC < delay_in_us) {
+		// hold
+	}
+	
+	TOTCR = 0x00; // disable timer as is
+}
+
+
+
+
+
