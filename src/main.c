eeff22778d78fbfd53853d9c25673be1a22b121f
1AmNotABear <z5359288@ad.unsw.edu.au>
2026-07-20 14:47:59 +1000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -14,6 +14,10 @@ int main(void) {
 
     homeState.roomLights |= LIGHT_BEDROOM2;
     updateLightState(&homeState);
+	
+		homeState.blind1 = BLIND_MID_WAY;    // blind 1 half up  -> LED1 green
+    homeState.blind2 = BLIND_ROLLED_UP;  // blind 2 full up  -> LED2 red
+    updateBlindState(&homeState);
     setupADC();
 
     while (1) {
@@ -21,21 +25,8 @@ int main(void) {
 
 
 
-
-
-        // update blind state based on adc, the numbers are 
-        // placeholders based on 10 bit adc.
-        unsigned int adcValue = readADC();
-        if (adcValue > 682) {
-            homeState.blind1 = BLIND_ROLLED_UP;
-        } else if (adcValue > 341) {
-            homeState.blind1 = BLIND_MID_WAY;
-        } else {
-            homeState.blind1 = BLIND_ROLLED_DOWN;
-        }
-        updateBlindState(&homeState);
-
     }
 
     return 0;
 }
+
